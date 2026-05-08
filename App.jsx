0ce6a06e0f243142ea2f84948a32c9d27cd9ab8b
pmvfs89-source <pmvfs89@gmail.com>
2026-05-08 15:33:50 +0100

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Academia Command Center</h1>
-      <p>Cola aqui o código do dashboard (App.jsx) que tens no ChatGPT.</p>
-    </div>
-  );
-}
